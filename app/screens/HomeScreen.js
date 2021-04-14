f801a73283369b12bd7a55d72d8e9476106f5eea
Alex Steffen <alex.steffen@cs-baupartner.de>
2021-04-14 09:08:33 +0200

    ➕  react-native-maps

--- app/screens/HomeScreen.js
+++ app/screens/HomeScreen.js
@@ -1,15 +1,35 @@
 import React from 'react';
-import { View, StyleSheet } from 'react-native';
-import { Title } from 'react-native-paper';
+import { View, StyleSheet, SafeAreaView } from 'react-native';
+import { Surface, Title, Text, FAB } from 'react-native-paper';
 
 const styles = StyleSheet.create({
   container: { flex: 1, justifyContent: 'center', alignItems: 'center' },
+  surface: {
+    padding: 8,
+    height: 200,
+    width: '100%',
+    alignItems: 'center',
+    justifyContent: 'center',
+    elevation: 4,
+  },
+  fab: {
+    position: 'absolute',
+    margin: 16,
+    right: 0,
+    bottom: 0,
+  },
 });
 
 export default function HomeScreen() {
   return (
-    <View style={styles.container}>
+    <SafeAreaView style={{ flex: 1, margin: 20 }}>
       <Title>Home!</Title>
-    </View>
+      <View style={styles.container}>
+        <Surface style={styles.surface}>
+          <Text>Placeholder!</Text>
+        </Surface>
+        <FAB style={styles.fab} small icon="plus" onPress={() => console.log('Pressed')} />
+      </View>
+    </SafeAreaView>
   );
 }
---
+++ app/screens/MapScreen.native.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { View, StyleSheet, Dimensions } from 'react-native';
+import { Title } from 'react-native-paper';
+import MapView from 'react-native-maps';
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  map: {
+    width: Dimensions.get('window').width,
+    height: Dimensions.get('window').height,
+  },
+});
+
+export default function MapScreen() {
+  return (
+    <View style={styles.container}>
+      <Title>Map!</Title>
+      <MapView style={styles.map} />
+    </View>
+  );
+}
